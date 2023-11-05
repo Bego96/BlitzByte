410ddfe7ac230102a955ba31154ca4e8d010c334
Bego96 <begic.ismar96@gmail.com>
2023-11-05 22:04:56 +0100

    Finnished basic implementation of search bar

--- src/components/search/resultItems.js
+++ src/components/search/resultItems.js
@@ -2,24 +2,10 @@ import { Link, useLocation } from "react-router-dom";
 
 function ResultItem(props) {
 
-    const location = useLocation();
-   
-    const pathnames = location.pathname.split('/').filter((item) => item);
     
-    const routeTo = `/${pathnames.slice(0, 1 + 1).join('/')}`;
-    
-    const route = `${routeTo}/${props.id}`
-   
-
-    const addToCart = (productId) => {
-        const product = productId;
-        console.log(product)
-        props.addToCart(product)
-    }
-
     return (
         <div className="bg-white rounded-md mb-4 flex justify-between items-center p-4 bg-slate-200">
-            <Link to={route}><div className="w-[70%] flex items-center">
+            <Link to={`/Shop/${props.id}`}><div className="w-[70%] flex items-center">
                 <div className="w-[20%] mr-4 tablet:w-full">
                     <img src={props.image[0].img} alt="produt"/>
                 </div>
